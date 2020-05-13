98031552445951f800138a771393730ae9104eac
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2020-05-13 20:24:26 -0300

    apiviacep

--- src/components/Viacep.js
+++ src/components/Viacep.js
@@ -1,23 +1,33 @@
 import React, { Component } from 'react';
-
+import api from '../services/api'
 // import { Container } from './styles';
 
 class Viacep extends Component {
     state = {
-
+        cep: ''
     }
 
-    apiViacep = () => {
-        
+    apiViacep = async () => {
+
+        const response = await api.get(`/ws/01001000/json/`)
+        console.log(response.data)
+        this.setState({
+            cep: response.data
+        })
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.apiViacep()
     }
 
     render() {
+        const { cep } = this.state;
         return (
             <>
+                <h5>Vagas Frontend</h5>
+                {cep.map(repo => (
+                    <p>{repo.cep}</p>
+                    ))}
             </>
         )
     }
